99a2b12bc76af185888b844c2f6e834ec55173ef
Dev Kaliper <gxlthz@gmail.com>
2023-04-16 22:03:00 -0500

    singin section done on mobile

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const register = document.getElementById('register');
 const sing_in = document.getElementById('sign_in');
+const singin = document.getElementById('singin');
 
 register.addEventListener('click', () => {
     document.getElementById("discover").classList.add("invisible");
@@ -8,4 +9,7 @@ register.addEventListener('click', () => {
 });
 sing_in.addEventListener('click', () => {
     document.getElementById("discover").classList.add("invisible");
+    document.getElementById("discover").classList.add("absolute");
+    singin.classList.remove("invisible");
+    singin.classList.remove("absolute");
 });
